0cbd93cde6eaa80cac1ea4b0fee41ab9edd4837a
rjyoung <rjyoung@ucsd.edu>
2022-05-12 22:22:43 -0700

    first commit

---
+++ ark_tribe_mgmt/src/main/java/com/example/ark_tribe_mgmt/ArkTribeMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.ark_tribe_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkTribeMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkTribeMgmtApplication.class, args);
+	}
+
+}
---
+++ ark_tribe_mgmt/src/test/java/com/example/ark_tribe_mgmt/ArkTribeMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ark_tribe_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkTribeMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
